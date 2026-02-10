efbde03ae2b000a13402e9be37d540b9f7cdb5f8
fangyq <245960548@qq.com>
2026-02-10 11:24:53 +0800

    全局异常处理

--- src/router/tool_config.py
+++ src/router/tool_config.py
@@ -8,6 +8,7 @@ tool_config_router = APIRouter(prefix="/tool_config", tags=["tool_config"])
 @tool_config_router.get("/price")
 async def search(request: Request):
     env = request.scope["env"]
-    return CommonResult.success(await env.DB.prepare(
+    data = await env.DB.prepare(
         "SELECT * FROM tool_config WHERE item_name like '%price'"
-    ).run().results)
+    ).run()
+    return CommonResult.success(data.results)
